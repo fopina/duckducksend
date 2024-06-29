57804a86eda6bd6f2fcb83cbb732aa8b3ef2bda0
Filipe Pina <hzlu1ot0@duck.com>
2024-06-29 20:27:18 +0100

    x

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,40 @@
 function _refactorEmail($data, $refs) {
-    if (!$refs.from.value.includes('@')) return;
-    if (!$refs.to.value.includes('@')) return;
+    let valid = true;
+    $data.fromError = '';
+    $data.toError = '';
+    if ($refs.from.value) {
+        if ($refs.from.value.includes('@') && !$refs.from.value.endsWith('@duck.com')) {
+            $data.fromError = "Not a @duck.com email address";
+            valid = false;
+        }
+    } else {
+        valid = false;
+    }
+    if ($refs.to.value) {
+        if (!$refs.to.value.includes('@')) {
+            $data.toError = "Not an email address";
+            valid = false;
+        }
+    } else {
+        valid = false;
+    }
+    if (!valid) return;
     let s = $refs.from.value.split('@')[0];
     let p = $refs.to.value.replace('@', '_at_');
     $data.refactoredEmail = `${p}_${s}@duck.com`;
+}
+
+let onCopy = () => {alert('Copied to clipboard')}
+
+const copy = (target) => {
+    if (typeof target === 'function') {
+        target = target()
+    }
+
+    if (typeof target === 'object') {
+        target = JSON.stringify(target)
+    }
+
+    return window.navigator.clipboard.writeText(target)
+        .then(onCopy)
 }
